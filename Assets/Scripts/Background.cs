378d0834c652c3189db1fdc710c4040c4f4de88a
EJ Mej <ejgmejilla@gmail.com>
2024-06-05 23:16:06 +0800

    Added basic player movement & camera drift

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed;
+    private float y;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.down * scrollSpeed;
+    }
+}
