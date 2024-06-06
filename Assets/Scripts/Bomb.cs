19b5bfc2e2a02fbaae454164081be29f35ae09e5
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2024-06-07 00:24:52 +0800

    Made the bomb do damage and added the explosion animation to it upon collision.

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed;
-    private float damageAmount = 20f; 
+    private float damageAmount = 20f;
+    private Animator myAnimator;
+    private Collider2D myCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -28,8 +31,24 @@ public class Bomb : MonoBehaviour
             // Drain stamina
             player.TakeDamage(damageAmount);
 
-            Destroy(gameObject);
+            // Disable the collider to prevent further collisions while it explodes 
+            myCollider.enabled = false;
+
+            // Explode animation
+            myAnimator.SetTrigger("collide");
+
+            // Start the coroutine to destroy the bomb after the animation
+            StartCoroutine(DestroyAfterAnimation());
         }
+    }
+
+    private IEnumerator DestroyAfterAnimation()
+    {
+        // Wait until the animation completes
+        yield return new WaitForSeconds(myAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Destroy the bomb object
+        Destroy(gameObject);
     }
 
     void OnBecameInvisible()
